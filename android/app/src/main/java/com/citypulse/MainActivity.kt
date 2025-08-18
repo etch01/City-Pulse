1d9740e402296a9569b94512e143a01920354b5b
Hassan Emad <h.taha@refundz.io>
2025-08-18 14:07:47 +0300

    init

--- android/app/src/main/java/com/citypulse/MainActivity.kt
+++ android/app/src/main/java/com/citypulse/MainActivity.kt
@@ -13,6 +13,10 @@ class MainActivity : ReactActivity() {
    */
   override fun getMainComponentName(): String = "CityPulse"
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
+  }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
