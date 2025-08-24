827726076c35175d382306a74375822fb7845ead
Hassan Emad <h.taha@refundz.io>
2025-08-24 11:58:34 +0300

    splash screen native

--- android/app/src/main/java/com/citypulse/MainActivity.kt
+++ android/app/src/main/java/com/citypulse/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.citypulse
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -8,18 +9,21 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 class MainActivity : ReactActivity() {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
    */
   override fun getMainComponentName(): String = "CityPulse"
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
+  override fun onCreate(savedInstanceState: Bundle?) {
+      // Switch back to AppTheme after showing SplashTheme
+      setTheme(R.style.AppTheme)
+      super.onCreate(null)
   }
+
   /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   * Returns the instance of the [ReactActivityDelegate].
+   * We use [DefaultReactActivityDelegate] which allows
+   * you to enable New Architecture with a single boolean flag [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
